a709a164d2b5d6f4f7cd150bc46f6cb0d1ebc1af
Mustafa Temel <m.mustafatemel@outlook.com>
2025-09-28 23:49:56 +0300

    Enable page view counter with Valine visitor tracking

--- exampleSite/static/js/page-counter.js
+++ exampleSite/static/js/page-counter.js
@@ -1,18 +1,32 @@
 // Simple page view counter using localStorage
-(function() {
+document.addEventListener('DOMContentLoaded', function() {
     const pageUrl = window.location.pathname;
     const storageKey = 'pageViews_' + btoa(pageUrl).replace(/[^a-zA-Z0-9]/g, '');
     
     // Get current view count
     let viewCount = parseInt(localStorage.getItem(storageKey) || '0');
     
-    // Increment view count
-    viewCount++;
-    localStorage.setItem(storageKey, viewCount.toString());
-    
-    // Display view count
-    const viewsElement = document.getElementById('page-views');
-    if (viewsElement) {
-        viewsElement.innerHTML = `<i class="far fa-eye fa-fw"></i> ${viewCount} görüntülenme`;
+    // Increment view count (only once per session per page)
+    const sessionKey = 'session_' + storageKey;
+    if (!sessionStorage.getItem(sessionKey)) {
+        viewCount++;
+        localStorage.setItem(storageKey, viewCount.toString());
+        sessionStorage.setItem(sessionKey, 'viewed');
     }
-})();
+    
+    // Display view count in multiple possible locations
+    const selectors = ['#page-views', '.leancloud_visitors', '.post-meta-views'];
+    
+    selectors.forEach(selector => {
+        const viewsElement = document.querySelector(selector);
+        if (viewsElement) {
+            viewsElement.innerHTML = `<i class="far fa-eye fa-fw" aria-hidden="true"></i>&nbsp;${viewCount}&nbsp;görüntülenme`;
+        }
+    });
+    
+    // Also try to find and update existing view counters
+    const existingCounters = document.querySelectorAll('.leancloud-visitors-count');
+    existingCounters.forEach(counter => {
+        counter.textContent = viewCount;
+    });
+});
